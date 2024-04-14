22006f3d0b92a2d1880a6d22f502a08d46634e9b
jaeyeon.kim <foodsksms@gmail.com>
2024-04-14 17:01:04 +0900

    feign 로그 수정

--- clients/client-sample/src/main/kotlin/me/jimmy/hellospring/clients/config/ExampleClientConfig.kt
+++ clients/client-sample/src/main/kotlin/me/jimmy/hellospring/clients/config/ExampleClientConfig.kt
@@ -1,8 +1,15 @@
 package me.jimmy.hellospring.clients.config
 
+import feign.Logger
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
-@EnableFeignClients
+@EnableFeignClients(basePackages = ["me.jimmy.hellospring"])
 @Configuration
-class ExampleClientConfig
+class ExampleClientConfig {
+    @Bean
+    fun feignLoggerLevel(): Logger.Level {
+        return Logger.Level.FULL
+    }
+}
