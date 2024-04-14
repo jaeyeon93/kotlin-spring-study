0d2dbdec5c755893907aaf0a6e44994705c27533
jaeyeon.kim <foodsksms@gmail.com>
2024-04-14 17:01:04 +0900

    feign 로그 수정

--- clients/client-sample/src/main/kotlin/me/jimmy/hellospring/clients/ExampleClient.kt
+++ clients/client-sample/src/main/kotlin/me/jimmy/hellospring/clients/ExampleClient.kt
@@ -1,6 +1,15 @@
 package me.jimmy.hellospring.clients
 
 import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 
-@FeignClient(value = "\${example-client.url}", name = "example-client")
-interface ExampleClient
+@FeignClient(url = "\${example-client.url}", name = "example-client")
+interface ExampleClient {
+    @GetMapping(
+        value = ["/users/{userId}"],
+        consumes = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun getUserById(@PathVariable userId: Long): UserResponseDto
+}
