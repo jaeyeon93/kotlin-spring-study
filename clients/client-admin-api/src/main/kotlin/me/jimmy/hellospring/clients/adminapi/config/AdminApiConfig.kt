b7bbc95738bdeaa171860af0c543d9c3fa7a5b3e
jaeyeon.kim <foodsksms@gmail.com>
2025-02-01 23:33:04 +0900

    Feature Feign retryer 구성 및 통테 작성

--- clients/client-admin-api/src/main/kotlin/me/jimmy/hellospring/clients/adminapi/config/AdminApiConfig.kt
+++ clients/client-admin-api/src/main/kotlin/me/jimmy/hellospring/clients/adminapi/config/AdminApiConfig.kt
@@ -1,8 +1,22 @@
 package me.jimmy.hellospring.clients.adminapi.config
 
+import feign.Retryer
+import feign.codec.ErrorDecoder
+import me.jimmy.hellospring.clients.adminapi.errordecoder.InternalServerErrorDecoder
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @EnableFeignClients(basePackages = ["me.jimmy.hellospring.clients.adminapi"])
 @Configuration
-internal class AdminApiConfig
+internal class AdminApiConfig {
+    @Bean
+    fun adminApiRetryer(): Retryer {
+        return Retryer.Default(1000, 3000, 3)
+    }
+
+    @Bean
+    fun adminApiErrorDecoder(): ErrorDecoder {
+        return InternalServerErrorDecoder()
+    }
+}
---
+++ clients/client-admin-api/src/test/kotlin/me/jimmy/hellospring/clients/adminapi/TestApplication.kt
@@ -0,0 +1,6 @@
+package me.jimmy.hellospring.clients.adminapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+internal class TestApplication
