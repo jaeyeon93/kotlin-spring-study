b1235951067e1851cfc0d675c20bf329c98b5061
jaeyeon.kim <foodsksms@gmail.com>
2025-03-08 23:49:31 +0900

    Feature Async 설정

---
+++ hello-spring-api/src/main/kotlin/me/jimmy/hellospring/config/AsyncConfiguration.kt
@@ -0,0 +1,46 @@
+package me.jimmy.hellospring.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.task.TaskDecorator
+import org.springframework.core.task.support.CompositeTaskDecorator
+import org.springframework.core.task.support.ContextPropagatingTaskDecorator
+import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+
+@Configuration
+@EnableAsync
+internal class AsyncConfiguration {
+    @Bean
+    fun coreApiTaskExecutor(): ThreadPoolTaskExecutor {
+        return ThreadPoolTaskExecutor().initExecutor(
+            corePoolSize = 30,
+            maxPoolSize = 30,
+            threadNamePrefix = "core-api-async",
+            taskDecorator = CompositeTaskDecorator(
+                listOf(
+                    ContextPropagatingTaskDecorator(),
+                    MdcDecorator(),
+                )
+            )
+        )
+    }
+
+    private fun ThreadPoolTaskExecutor.initExecutor(
+        corePoolSize: Int,
+        maxPoolSize: Int,
+        threadNamePrefix: String,
+        awaitTerminationSeconds: Int = 3,
+        waitForJobToCompleteOnShutDown: Boolean = true,
+        taskDecorator: TaskDecorator,
+    ): ThreadPoolTaskExecutor {
+        return apply {
+            setCorePoolSize(corePoolSize)
+            setMaxPoolSize(maxPoolSize)
+            setThreadNamePrefix(threadNamePrefix)
+            setAwaitTerminationSeconds(awaitTerminationSeconds)
+            setWaitForTasksToCompleteOnShutdown(waitForJobToCompleteOnShutDown)
+            setTaskDecorator(taskDecorator)
+        }
+    }
+}
---
+++ hello-spring-api/src/main/kotlin/me/jimmy/hellospring/controller/AsyncController.kt
@@ -0,0 +1,39 @@
+package me.jimmy.hellospring.controller
+
+import me.jimmy.hellospring.clients.adminapi.AdminApi
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.Future
+
+@RestController
+internal class AsyncController(
+    private val coreApiTaskExecutor: ThreadPoolTaskExecutor,
+    private val adminApi: AdminApi,
+) {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    @GetMapping("/async-test/{count}")
+    private fun asyncTest(@PathVariable count: Int): String {
+        logger.info("AsyncController#asyncTest start")
+        val futures = mutableListOf<Future<String>>()
+        for (i in 1..count) {
+            futures.add(coreApiTaskExecutor.submit<String> { adminApi.test() })
+        }
+        futures.forEach { it.get() }
+        logger.info("AsyncController#asyncTest end")
+        return "OK"
+    }
+
+    @GetMapping("/sync-test/{count}")
+    private fun syncTest(@PathVariable count: Int): String {
+        logger.info("AsyncController#syncTest start")
+        for (i in 1..count) {
+            adminApi.test()
+        }
+        logger.info("AsyncController#syncTest end")
+        return "OK"
+    }
+}
